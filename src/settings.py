26a34164dc46d97bb85516546d2205139ba8424b
Paolo Stivanin <info@paolostivanin.com>
2014-10-06 14:58:27 +0200

    updated files

--- src/settings.py
+++ src/settings.py
@@ -5,59 +5,44 @@ MONGO_USERNAME = 'user'
 MONGO_PASSWORD = 'pass'
 MONGO_DBNAME = 'apitest'
 
-# /people
+# /vms
 RESOURCE_METHODS = ['GET', 'POST', 'DELETE']
 
-# /people/<id>
+# /vms/<id>
 ITEM_METHODS = ['GET', 'PATCH', 'PUT', 'DELETE']
 
 schema = {
     # Schema definition, based on Cerberus grammar. Check the Cerberus project
     # (https://github.com/nicolaiarocci/cerberus) for details.
-    'firstname': {
+    'name': {
         'type': 'string',
         'minlength': 1,
-        'maxlength': 10,
+        'maxlength': 20,
+        'required': True,
     },
-    'lastname': {
+    'os': {
         'type': 'string',
         'minlength': 1,
         'maxlength': 15,
         'required': True,
-        # talk about hard constraints! For the purpose of the demo
-        # 'lastname' is an API entry-point, so we need it to be unique.
-        'unique': True,
     },
-    # 'role' is a list, and can only contain values from 'allowed'.
-    'role': {
-        'type': 'list',
-        'allowed': ["author", "contributor", "copy"],
-    },
-    # An embedded 'strongly-typed' dictionary.
-    'location': {
-        'type': 'dict',
-        'schema': {
-            'address': {'type': 'string'},
-            'city': {'type': 'string'}
-        },
-    },
-    'born': {
+    'deployed': {
         'type': 'datetime',
     },
 }
 
-people = {
+vms = {
     # 'title' tag used in item links. Defaults to the resource title minus
-    # the final, plural 's' (works fine in most cases but not for 'people')
-    'item_title': 'person',
+    # the final, plural 's' (works fine in most cases but not for 'vms')
+    'item_title': 'vm',
 
     # by default the standard item entry point is defined as
-    # '/people/<ObjectId>'. We leave it untouched, and we also enable an
+    # '/vms/<ObjectId>'. We leave it untouched, and we also enable an
     # additional read-only entry point. This way consumers can also perform
-    # GET requests at '/people/<lastname>'.
+    # GET requests at '/vms/<os>'.
     'additional_lookup': {
         'url': 'regex("[\w]+")',
-        'field': 'lastname'
+        'field': 'os'
     },
 
     # We choose to override global cache-control directives for this resource.
@@ -71,5 +56,5 @@ people = {
 }
 
 DOMAIN = {
-    'people': people,
+    'vms': vms,
 }
